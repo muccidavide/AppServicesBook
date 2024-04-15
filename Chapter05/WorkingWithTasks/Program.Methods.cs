140034525ae44269310a07d99c580df7b305e1dd
Davide Mucci <davidemucci0804@gmail.com>
2024-04-15 22:48:27 +0200

    Continuing with another task

---
+++ Chapter05/WorkingWithTasks/Program.Methods.cs
@@ -0,0 +1,42 @@
+﻿partial class Program
+{
+    private static void MethodA()
+    {
+        TaskTitle("Starting Method A...");
+        OutputThreadInfo();
+        Thread.Sleep(3000); // Simulate three seconds of work.
+        TaskTitle("Finished Method A.");
+    }
+    private static void MethodB()
+    {
+        TaskTitle("Starting Method B...");
+        OutputThreadInfo();
+        Thread.Sleep(2000); // Simulate two seconds of work.
+        TaskTitle("Finished Method B.");
+    }
+    private static void MethodC()
+    {
+        TaskTitle("Starting Method C...");
+        OutputThreadInfo();
+        Thread.Sleep(1000); // Simulate one second of work.
+        TaskTitle("Finished Method C.");
+    }
+
+    private static decimal CallWebService()
+    {
+        TaskTitle("Strating call to web serivice");
+        OutputThreadInfo();
+        Thread.Sleep(Random.Shared.Next(2000, 4000));
+        TaskTitle("Finished call to web service");
+        return 89.99M;
+    }
+
+    private static string CallStoreProcedure(decimal amount)
+    {
+        TaskTitle("Starting call to store procedure...");
+        OutputThreadInfo();
+        Thread.Sleep((Random.Shared.Next(2000, 4000)));
+        TaskTitle("Finished call to store procedure...");
+        return $"12 products cost more than {amount:C}.";
+    }
+}
---
+++ Chapter05/WorkingWithTasks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics; // To use Stopwatch.
+using static System.Console;
+
+
+OutputThreadInfo();
+Stopwatch timer = Stopwatch.StartNew();
+
+SectionTitle("Running methods synchronously on one thread.");
+MethodA();
+MethodB();
+MethodC();
+
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+timer.Restart();
+
+SectionTitle("Running methods asynchronously on multiple threads");
+
+Task taskA = new(MethodA);
+taskA.Start();
+Task taskB = Task.Factory.StartNew(MethodB);
+Task taskC = Task.Run(MethodC);
+
+Task[] tasks = { taskA, taskB, taskC };
+Task.WaitAll(tasks);
+
+timer.Restart();
+
+SectionTitle("Passing results of the one task as an input into another.");
+
+Task<string> taskServiceThenSProc = Task.Factory
+    .StartNew(CallWebService) // returns Task<decimal>
+    .ContinueWith(previousTask => // return Task<string>
+    CallStoreProcedure(previousTask.Result));
+
+
+WriteLine($"Result: {taskServiceThenSProc.Result}");
+
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
