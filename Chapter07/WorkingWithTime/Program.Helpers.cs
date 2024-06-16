97743de9eb4cbc570fffd57db35208206f5a9ec4
Davide Mucci <davidemucci0804@gmail.com>
2024-06-16 11:52:59 +0200

    partial chp 7

---
+++ Chapter07/WorkingWithTime/Program.Helpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+partial class Program
+{
+    private static void ConfigureConsole(string culture = "en-US",
+        bool overrideComputerCulture = true)
+    {
+        // TO enable special charachters like Euro currency symbol
+        OutputEncoding = Encoding.UTF8;
+        Thread t = Thread.CurrentThread;
+        if (overrideComputerCulture)
+        {
+            t.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+            t.CurrentUICulture = t.CurrentCulture;
+        }
+
+        CultureInfo ci = t.CurrentCulture;
+        WriteLine($"Current Culture: {ci.DisplayName}");
+        WriteLine($"Short date pattern: {ci.DateTimeFormat.ShortDatePattern}");
+        WriteLine($"Long date pattern: {ci.DateTimeFormat.LongDatePattern}");
+        WriteLine();
+    }
+
+    private static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"*** {title}");
+        ForegroundColor = previousColor;
+    }
+}
+
---
+++ Chapter07/WorkingWithTime/Program.cs
@@ -0,0 +1,24 @@
+﻿
+    
+/*ConfigureConsole(); // Defaults to en-US culture.
+SectionTitle("Specifying date and time values");
+WriteLine($"DateTime.MinValue: {DateTime.MinValue}");
+WriteLine($"DateTime.MaxValue: {DateTime.MaxValue}");
+WriteLine($"DateTime.UnixEpoch: {DateTime.UnixEpoch}");
+WriteLine($"DateTime.Now: {DateTime.Now}");
+WriteLine($"DateTime.Today: {DateTime.Today}");
+WriteLine($"DateTime.Today: {DateTime.Today:d}");
+WriteLine($"DateTime.Today: {DateTime.Today:D}");*/
+
+DateTime xmas = new DateTime(year: 2024, month: 12, day: 25);
+WriteLine($"Chirstmas (default format): {xmas}");
+WriteLine($"Chirstmas (custom short format): {xmas:ddd d/M/yy}");
+WriteLine($"Chirstmas (custom long format): {xmas:dddd, dd MMMM yyyy}");
+WriteLine($"Chirstmas (standard long format): {xmas:D}");
+WriteLine($"Chirstmas (sortable): {xmas:u}");
+WriteLine($"Chirstmas month: {xmas.Month}");
+WriteLine($"Chirstmas day: ");
+WriteLine($"Chirstmas day {xmas.DayOfYear} of the year {xmas.Year} and Year");
+WriteLine($"Chirstmas {xmas.Year} is on {xmas.DayOfWeek}");
+
+// continue page 260
